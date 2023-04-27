48e770d19d2e261e9edf41735c5b137886bc8a0e
Harsh Kapadia <harshgkapadia@gmail.com>
2023-04-26 23:52:29 -0600

    Initial commit

---
+++ copy-files-config-homa.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+cp -r ~/HomaModule/cloudlab/bin ~/bin
+cp ~/HomaModule/homa.ko ~/HomaModule/util/cp_node ~/HomaModule/util/homa_prio ~/HomaModule/util/*.py ~/bin
+
+~/bin/config default
+
