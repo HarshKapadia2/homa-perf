2ce89a8de878a4c63f3632dde1c13c491a78933d
harshk0 <harshk0@node0.homa-test-14.cloudlab-pg0.utah.cloudlab.us>
2023-04-27 07:39:45 -0600

    Reduce script functionality to only copy files

--- copy-homa-files.sh
+++ copy-homa-files.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-
 cp -r ~/HomaModule/cloudlab/bin ~/bin
 cp ~/HomaModule/homa.ko ~/HomaModule/util/cp_node ~/HomaModule/util/homa_prio ~/HomaModule/util/*.py ~/bin
-
-~/bin/config default
 
